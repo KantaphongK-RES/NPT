3200872a3c581497f553d87e35dcb3be096d4ecb
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-02 01:29:14 +0700

    add logger

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
+const logger = require("./middlewares/logger.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const controllersNotes = require("./routes/notes.js");
+app.use(logger);
 app.use("/api/v1/Notes", controllersNotes);
 app.listen(
   PORT,
@@ -12,3 +14,4 @@ app.listen(
     `server listening on ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
+// keep it cleanest as possible
