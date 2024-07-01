2aedc06cd49f834f8835c4771696cf70f134f90d
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-02 01:34:42 +0700

    add morgan

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
-const logger = require("./middlewares/logger.js");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const controllersNotes = require("./routes/notes.js");
-app.use(logger);
+if (process.env.NODE_ENV === "Development") {
+  app.use(morgan("dev"));
+}
 app.use("/api/v1/Notes", controllersNotes);
 app.listen(
   PORT,
