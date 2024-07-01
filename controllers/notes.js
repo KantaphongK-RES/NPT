c56cad5276d93052c54024852e83e5667229fd96
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-02 06:03:17 +0700

    learning that order matter

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,23 +2,36 @@ const express = require("express");
 const Note = require("../models/Note");
 const router = express.Router();
 // get all notes, GET /api/v1/notes, public
-exports.getAllNotesData = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: "got all notes data" });
+exports.getAllNotesData = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json({ succes: true, data: notes });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
-// get all notes per id, GET /api/v1/notes/:id, public
-exports.getNoteData = (req, res, next) => {
-  res.status(200).json({ succes: true, msg: "got note data" });
+// get notes per id, GET /api/v1/notes/:id, public
+exports.getNoteData = async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    res.status(200).json({ succes: true, data: note });
+    if (!note) {
+      res.status(400).json({ success: false });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // create note, POST /api/v1/notes, private
 exports.postNoteData = async (req, res, next) => {
-  // try {
-  const note = await Note.create(req.body);
-  res.status(200).json({ succes: true, data: note });
-  // } catch (err) {
-  //   res.status(400).json({ success: false });
-  // }
+  try {
+    const note = await Note.create(req.body);
+    res.status(200).json({ succes: true, data: note });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // Update note data, PUT /api/v1/notes/:id, private
