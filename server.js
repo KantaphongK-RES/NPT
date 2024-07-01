3060c7b7ac49afc10d1acbf617defbda11ad1342
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-02 04:14:35 +0700

    added  console colors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config({ path: "./config/config.env" });
 const morgan = require("morgan");
 const connectDB = require("./config/db.js");
 connectDB();
+const colors = require("colors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,11 +13,15 @@ if (process.env.NODE_ENV === "Development") {
   app.use(morgan("dev"));
 }
 app.use("/api/v1/Notes", controllersNotes);
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `server listening on ${process.env.NODE_ENV} mode on port ${PORT}`
+    `server listening on ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
   )
 );
+process.on("unhandleRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
 // keep it cleanest as possible
 // add environment on post man
