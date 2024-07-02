5b4ca521f7d41214dce05d3325aa885d20dd47b4
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-03 00:28:51 +0700

    file upload logic

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const Note = require("../models/Note");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
@@ -59,7 +60,7 @@ exports.deleteNote = asyncHandler(async (req, res, next) => {
 //4.Update note data, PUT    /api/v1/notes/:id  , private
 //5.Delete note data, DELETE /api/v1/notes/:id  , private
 
-//  CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD 
+//  CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD CRUD
 
 //6.
 exports.photoUpload = asyncHandler(async (req, res, next) => {
@@ -73,8 +74,31 @@ exports.photoUpload = asyncHandler(async (req, res, next) => {
     );
   }
   if (!req.files) {
-    return next(new ErrorResponse(`please attach the photo file(s) `, 400));
+    return next(new ErrorResponse(`please attach the file(s) `, 400));
   }
+  const file = req.files.file;
+  if (!file.mimetype.startsWith("image")) {
+    return next(
+      new ErrorResponse(`please attach the actually photo file(s) `, 400)
+    );
+  }
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `please attach the photo file(s) that size is/are less than ${process.env.MAX_FILE_UPLOAD} `,
+        400
+      )
+    );
+  }
+  file.name = `photo_${Note._id}${path.parse(file.name).ext}`;
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.error(err);
+      return next(new ErrorResponse(`Problem with upload file(s)`, 500));
+    }
+    await Note.findByIdAndUpdate(req.params.id, { photo: file.name });
+    res.status(200).json({ success: true, data: file.name });
+  });
 });
 //6.Upload note photo, PUT /api/v1/notes/:id/photo  , private
 
