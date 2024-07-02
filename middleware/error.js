9ca7dc69153354b51887421e3b10c74f31df4445
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-02 15:08:16 +0700

    added error handler for duplicate name of note

--- middleware/error.js
+++ middleware/error.js
@@ -3,10 +3,13 @@ const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
-
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+  if (err.code === 11000) {
+    const message = `Duplicate resource with name of note ${req.body.notename} existed`;
+    error = new ErrorResponse(message, 400);
   }
   console.log(err.stack.red);
   res
