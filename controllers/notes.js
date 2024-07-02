2bd0751738214ceb85c55fb54dda74a268dad726
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-02 16:23:43 +0700

    asyncmiddleware

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,83 +1,60 @@
 const Note = require("../models/Note");
 const ErrorResponse = require("../utils/errorResponse");
-// get all notes, GET /api/v1/notes, public
-exports.getAllNotesData = async (req, res, next) => {
-  try {
-    const notes = await Note.find();
-    if (!notes) {
-      return next(err);
-    }
-    res.status(200).json({ succes: true, data: notes, count: notes.length });
-  } catch (err) {
-    next(err);
+const asyncHandler = require("../middleware/async");
+// 1.
+exports.getAllNotesData = asyncHandler(async (req, res, next) => {
+  const notes = await Note.find();
+  if (!notes) {
+    return next(err);
   }
-};
+  res.status(200).json({ succes: true, data: notes, count: notes.length });
+});
 
-// get notes per id, GET /api/v1/notes/:id, public
-exports.getNoteData = async (req, res, next) => {
-  try {
-    const note = await Note.findById(req.params.id);
-    if (!note) {
-      return next(
-        new ErrorResponse(
-          `Note not found with that id of ${req.params.id}`,
-          404
-        )
-      );
-    }
-    res.status(200).json({ succes: true, data: note });
-  } catch (err) {
-    next(err);
+//2
+exports.getNoteData = asyncHandler(async (req, res, next) => {
+  const note = await Note.findById(req.params.id);
+  if (!note) {
+    return next(
+      new ErrorResponse(`Note not found with that id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ succes: true, data: note });
+});
 
-// create note, POST /api/v1/notes, private
-exports.postNoteData = async (req, res, next) => {
-  try {
-    const note = await Note.create(req.body);
-    res.status(200).json({ succes: true, data: note });
-  } catch (err) {
-    next(err);
-  }
-};
+//3.
+exports.postNoteData = asyncHandler(async (req, res, next) => {
+  const note = await Note.create(req.body);
+  res.status(200).json({ succes: true, data: note });
+});
 
-// Update note data, PUT /api/v1/notes/:id, private
-exports.editNoteData = async (req, res, next) => {
-  try {
-    const editnote = await Note.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!editnote) {
-      return next(
-        new ErrorResponse(
-          `Note not found with that id of ${req.params.id}`,
-          404
-        )
-      );
-    }
-    res.status(200).json({ succes: true, data: editnote });
-  } catch (err) {
-    next(err);
+//4.
+exports.editNoteData = asyncHandler(async (req, res, next) => {
+  const editnote = await Note.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!editnote) {
+    return next(
+      new ErrorResponse(`Note not found with that id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ succes: true, data: editnote });
+});
 
-// Delete note data, DELETE /api/v1/notes/:id, private
-exports.deleteNote = async (req, res, next) => {
-  try {
-    const delnote = await Note.findByIdAndDelete(req.params.id);
-    if (!delnote) {
-      return next(
-        new ErrorResponse(
-          `Note not found with that id of ${req.params.id}`,
-          404
-        )
-      );
-    }
-    res
-      .status(200)
-      .json({ succes: true, data: {}, msg: "Note deleted successfully" });
-  } catch (err) {
-    next(err);
+//5.
+exports.deleteNote = asyncHandler(async (req, res, next) => {
+  const delnote = await Note.findByIdAndDelete(req.params.id);
+  if (!delnote) {
+    return next(
+      new ErrorResponse(`Note not found with that id of ${req.params.id}`, 404)
+    );
   }
-};
+  res
+    .status(200)
+    .json({ succes: true, data: {}, msg: "Note deleted successfully" });
+});
+//1.get all notes   , GET    /api/v1/notes      , public
+//2.get notes per id, GET    /api/v1/notes/:id  , public
+//3.create note     , POST   /api/v1/notes      , private
+//4.Update note data, PUT    /api/v1/notes/:id  , private
+//5.Delete note data, DELETE /api/v1/notes/:id  , private
