74a39a391262102a761a2aa6d9e1b6a87286e399
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-03 14:01:06 +0700

    sign in and validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,5 +5,22 @@ const User = require("../models/User");
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
   const user = await User.create({ name, email, password, role });
-  res.status(200).json({ success: true });
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
+});
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(
+      new ErrorResponse("Please  provide an email and password", 400)
+    );
+  }
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid password", 401));
+  }
+  const user = await this.user.findOne({ email }.select("+password"));
+  if (!user) {
+    return next(new ErrorResponse("Invalid token", 401));
+  }
 });
