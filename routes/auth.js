2f058b673b5b5d72335d59c84c1ce281d9727a31
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-03 21:29:58 +0700

    fix wrongly put add permission

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const { register, login, getHimselfNotes } = require("../controllers/auth");
 const router = express.Router();
 const { permissionProtect } = require("../middleware/authpermission");
 
-router.get("/me", getHimselfNotes);
+router.get("/me", permissionProtect, getHimselfNotes);
 router.post("/register", register);
-router.post("/login", permissionProtect, login);
+router.post("/login", login);
 
 module.exports = router;
