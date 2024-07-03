ff20924b4a4f49f5bd02ab7077e95031d500a734
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-03 15:27:30 +0700

    cookies on controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,3 +26,16 @@ exports.login = asyncHandler(async (req, res, next) => {
   const token = user.getSignedJwtToken();
   res.status(200).json({ success: true, token });
 });
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60
+    ),
+    httpOnly: true,
+  };
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
