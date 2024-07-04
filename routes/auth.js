2bf4c6c45fd5537d24e71f4e9fe17752b415e984
Kantaphong <kantaphongkanyaruean.work@gmail.com>
2024-07-04 12:35:14 +0700

    add route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { register, login, getHimselfNotes } = require("../controllers/auth");
+const {
+  register,
+  login,
+  getHimselfNotes,
+  logout,
+} = require("../controllers/auth");
 const router = express.Router();
 const { permissionProtect } = require("../middleware/authpermission");
 
 router.get("/me", permissionProtect, getHimselfNotes);
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 
 module.exports = router;
